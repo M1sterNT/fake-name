a0aae1683d426fc8389004d8a93e3f88d78c54d8
wandee1997 <wandee016@gmail.com>
2018-12-20 17:42:01 +0700

    add inbox

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = (process.env.PORT || '3000');
 const getFake = require('./controllers/fake.controller');
 const getInbox = require('./controllers/email.controller');
 
-app.get('/', getFake)
-app.get('/inbox/:email', getInbox)
+app.all('/', getFake)
+app.all('/inbox/:email', getInbox)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
