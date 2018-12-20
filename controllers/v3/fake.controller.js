e89bb6debc5ae190834a5b67d497e197a8797753
wandee1997 <wandee016@gmail.com>
2018-12-21 00:47:11 +0700

    add v3

--- controllers/v3/fake.controller.js
+++ controllers/v3/fake.controller.js
@@ -2,10 +2,15 @@ const Fakerator = require("fakerator");
 
 const getFake = function (req, res) {
     const fakerator = Fakerator();
+    let firstName = fakerator.names.firstName()
+    let lastName = fakerator.names.lastName()
     let parameter = {
-        firstName:fakerator.names.firstName(),
-        lasttName:fakerator.names.lastName(),
-        email:fakerator.internet.email(),
+        firstName:firstName,
+        lastName:lastName,
+        email:fakerator.internet.email(firstName,lastName   ),
+        password:fakerator.internet.password(10),
+        ip:fakerator.internet.ip(),
+        mac:fakerator.internet.mac(),
         avatar:fakerator.internet.avatar()	
     }
     res.status(200).json(parameter);
