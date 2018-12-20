6722d5d97333ef25983dadef0e124cbe7759227e
wandee1997 <wandee016@gmail.com>
2018-12-20 13:08:11 +0700

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = normalizePort(process.env.PORT || '3000');
 const getFake = require('./controllers/fake.controller');
 app.get('/', getFake)
 
