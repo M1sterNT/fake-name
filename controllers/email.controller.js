ba63f52f274baf94c946b1b7961d12be8067323e
wandee1997 <wandee016@gmail.com>
2018-12-30 22:41:44 +0700

    fix email inbox

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -2,29 +2,49 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const axios = require('axios');
 const axiosCookieJarSupport = require('axios-cookiejar-support').default;
-const { CookieJar } = require('tough-cookie');
+//const { CookieJar } = require('tough-cookie');
 const { throttleAdapterEnhancer } = require('axios-extensions');
-const jar = new CookieJar();
+//const jar = new CookieJar();
+const unirest = require('unirest');
 
 
 const getInbox = async function (req, res) {
-    try {
-        let instance = axios.create({ baseURL: 'https://emailfake.com', 	headers: { 'Cache-Control': 'no-cache' },
-        adapter: throttleAdapterEnhancer(axios.defaults.adapter, 2 * 1000), withCredentials: true, jar });
-        axiosCookieJarSupport(instance)
-        let result = await instance.get('/' + req.params.email, { headers: { 'Cache-Control': 'no-cache' },
-        adapter: throttleAdapterEnhancer(axios.defaults.adapter, 2 * 1000), withCredentials: true, jar });
-        const dom = new JSDOM(result.data);
-        let link = dom.window.document.getElementsByClassName("mb_blk")[0].getElementsByTagName("a")[0].href
-        let parameter = {
-            status: true,
-            link: link,
-            email:unescape(link.split("e=")[1].split("&")[0]),
-            code: link.split("c=")[1].split("&")[0]
-        }
-         res.status(200).json(parameter);
+    
+        var CookieJar = unirest.jar();
+        unirest.get("https://emailfake.com/" +req.params.email).jar(CookieJar).end(function (response) {
+            try {
+            const dom = new JSDOM(response.body);
+            let link = dom.window.document.getElementsByClassName("mb_blk")[0].getElementsByTagName("a")[0].href
+            let parameter = {
+                status: true,
+                link: link,
+                email: unescape(link.split("e=")[1].split("&")[0]),
+                code: link.split("c=")[1].split("&")[0]
+            }
+            return res.status(200).json(parameter);
         } catch (error) {
-         res.status(200).json({ status: false });
-    }
+            return res.status(200).json({ status: false });
+         }
+        })
+   
+    /*  try {
+          let instance = axios.create({ baseURL: 'https://emailfake.com', 	headers: { 'Cache-Control': 'no-cache' },
+          adapter: throttleAdapterEnhancer(axios.defaults.adapter, 2 * 1000), withCredentials: true, jar });
+          axiosCookieJarSupport(instance)
+          let result = await instance.get('/' + req.params.email, { headers: { 'Cache-Control': 'no-cache' },
+          adapter: throttleAdapterEnhancer(axios.defaults.adapter, 2 * 1000), withCredentials: true, jar });
+          const dom = new JSDOM(result.data);
+          let link = dom.window.document.getElementsByClassName("mb_blk")[0].getElementsByTagName("a")[0].href
+          let parameter = {
+              status: true,
+              link: link,
+              email:unescape(link.split("e=")[1].split("&")[0]),
+              code: link.split("c=")[1].split("&")[0]
+          }
+           res.status(200).json(parameter);
+          } catch (error) {
+           res.status(200).json({ status: false });
+      } */
+
 }
 module.exports = getInbox;
